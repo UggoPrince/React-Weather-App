077aa4a6d9bc48e2db49626c6f7ded6aebee3656
uggoprince <uggoprince@gmail.com>
2019-09-18 03:09:58 +0100

    chore(redux): add redux
    - install redux
    - install react-redux
    
    [Finishes #168536421]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+
+const initialState = {
+  page: 'home',
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SETTINGS':
+      state.page = 'settings';
+      return { page: state.page };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 const title = 'React with Webpack and Babel';
+const rootEl = document.getElementById('app');
+
 ReactDOM.render(
-  <App title={title} />,
-  document.getElementById('app')
+  <Provider store={store}>
+    <App title={title} />
+  </Provider>, rootEl,
 );
